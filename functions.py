5e8badd6a2b75ce39ff1e7af473be491f507d9ac
Zhao Li <zhaol@hawaii.edu>
2020-11-25 12:18:17 -1000

    demonstrated function arguments

--- functions.py
+++ functions.py
@@ -1,4 +1,31 @@
-def my_function():
-  print("Hello from a function")
+def my_function(first_name = "John", last_name = "Doe"):
+  print(f"Hello {first_name} {last_name} from a function")
 
-my_function()
+my_function( # keyword arguments are more flexible and readable
+    last_name = 'Flinstone',
+    first_name = 'Fred',
+)
+
+my_function('Flinstone', 'Fred') # positional arguments are more brittle and rigid
+
+my_function() # use defaults
+
+
+
+def function_using_more_organization(
+    person = {
+        "first": "John",
+        "last": "Doe",
+    },
+    address = {
+        "number": "123",
+        "street": "Main St",
+    }
+):
+  print(f"Hello {person['first']} of {address['street']} from a more organized function")
+
+function_using_more_organization(
+    person = {
+        "first": "Jane"
+    }
+)
